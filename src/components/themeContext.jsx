4638d747fdf14bab61b13bf27cd39cbd153fcff9
JoseGalarza2000 <josepompilio2000@gmail.com>
2024-11-25 14:37:20 -0500

    25-11-2024 14:37

--- src/components/themeContext.jsx
+++ src/components/themeContext.jsx
@@ -1,22 +1,28 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import { ThemeProvider } from '@mui/material';
 import { darkThemeMui, themeMui } from './themesMui';
 
+// Crear el contexto
 const ThemeContext = createContext();
 
+// Hook personalizado para usar el contexto
 export const useTheme = () => useContext(ThemeContext);
 
+// Proveedor de Tema
 export const ThemeProviderBootstrap = ({ children }) => {
-    const [theme, setTheme] = useState('dark');
+    // Obtener el tema inicial desde las preferencias del sistema
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const [theme, setTheme] = useState(prefersDarkMode ? 'dark' : 'light');
     const [animationSkeleton] = useState('wave');
 
-    // Cambiar el tema globalmente
-    const rootElement = document.documentElement;
-    rootElement.setAttribute('data-bs-theme', theme); // Establece el tema inicial
+    // Actualizar el atributo `data-bs-theme` cuando cambie el tema
+    useEffect(() => {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+    }, [theme]);
 
+    // Función para alternar el tema
     const toggleTheme = () => {
         setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
-        rootElement.setAttribute('data-bs-theme', theme);
     };
 
     return (
